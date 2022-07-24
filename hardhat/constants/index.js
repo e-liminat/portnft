63e3ac5a71bd7032e4f41bf118fe3aae36f303c9
e-liminat <18430004+e-liminat@users.noreply.github.com>
2022-07-23 22:33:20 -0700

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x315CB4476d254F753328FcCe7AF3F6E91E330a05";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://portnft.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
